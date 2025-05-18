5c390b70c6128bab9b37fc89e86f2e54bc8cd6c6
badshahgames <lolxgaming484@gmail.com>
2025-05-18 09:45:06 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -12,6 +12,8 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
-export const provider = new GoogleAuthProvider();
+const auth = getAuth(app);
+const db = getFirestore(app);
+const provider = new GoogleAuthProvider();
+
+export { auth, db, provider };
